176b19d9668e699994d25aca07bf61a47a2c57d5
Kasia Tomaszewska <katarzynatomaszewska123@gmail.com>
2020-08-31 19:55:53 +0000

    describe excercises

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log('hello')
+//Fill this aray with things you like (strings)
+const kasiasFavoriteFoods = []
+
+//
+function doesSheLikeThis(food) {
+    //Logic to determine if passed food is in her list or not
+    //print true/false
+}
+
+function addToFavorites(food) {
+    //Logic to add passed item to list
+    //Print "Added <food>"
+    //Print list after add
+}
+
+function getRandomFood(){
+    //Prints random item from list
+}
+
+
+//TESTS DO NOT MODIFY!!
+
+doesSheLikeThis("papierosy")
+doesSheLikeThis("cake")
+doesSheLikeThis("vapiano")
+
+addToFavorites("kiwi")
+addToFavorites("arepas")
+addToFavorites("bao")
+
+getRandomFood()
+getRandomFood()
+getRandomFood()
