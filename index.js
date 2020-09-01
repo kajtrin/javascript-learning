b6c624ae40a157772da98046a371ab0f09c2504d
Kasia Tomaszewska <katarzynatomaszewska123@gmail.com>
2020-09-01 21:35:23 +0000

    implements does she like it function and add function

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 //Fill this aray with things you like (strings)
-const kasiasFavoriteFoods = []
+const kasiasFavoriteFoods = ["pesto","vegetables","nachos","arepas","pizza","cake"]
 
 //
-function doesSheLikeThis(food) {
+function doesSheLikeThis(food) { console.log(kasiasFavoriteFoods.includes(food)) }
+
     //Logic to determine if passed food is in her list or not
     //print true/false
-}
+
 
 function addToFavorites(food) {
     //Logic to add passed item to list
     //Print "Added <food>"
     //Print list after add
+
+    kasiasFavoriteFoods.push(food)
+    console.log("add " + food)
+    console.log(kasiasFavoriteFoods)
+
 }
 
 function getRandomFood(){
